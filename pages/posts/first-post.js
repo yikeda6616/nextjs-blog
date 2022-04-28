fbbae34cec98daad626650c27939b01e73520fee
Yasushi Ikeda <yikeda6616@gmail.com>
2022-04-28 19:51:18 +0900

    3-5: Remove example script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import Link from "next/link"
-import Script from "next/script"
 
 export default function FirstPost() {
   return (
@@ -8,11 +7,6 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload" // Load this particular script lazily during browser idle time
-        onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)} // Run any JavaScript code immediately after the script has finished loading.
-      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
