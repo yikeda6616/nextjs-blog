38512a309de6dd52ff8264afc33c1b895ad5ae28
Yasushi Ikeda <yikeda6616@gmail.com>
2022-04-28 19:50:58 +0900

    3-5: Using the Script Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Link from "next/link"
+import Script from "next/script"
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,11 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload" // Load this particular script lazily during browser idle time
+        onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)} // Run any JavaScript code immediately after the script has finished loading.
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
