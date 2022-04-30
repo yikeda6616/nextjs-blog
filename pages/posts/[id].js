53a8870ec8a61e7c0e87b4e62be90cb90e514623
Yasushi Ikeda <yikeda6616@gmail.com>
2022-04-30 16:25:46 +0900

    5-9: Fallback

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -19,7 +19,7 @@ export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
     paths,
-    fallback: false
+    fallback: false // any paths not returned by getStaticPaths will result in a 404 page.
   }
 }
 
