2f092ec8f38f1cdb98aebb4ee0331e4f52c51d2b
Yasushi Ikeda <yikeda6616@gmail.com>
2022-04-30 16:13:44 +0900

    5-7: Formatting the Date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
 
 // The post page is now using the getPostData function in getStaticProps to get the post data and return it as props.
 export async function getStaticProps({ params }) {
@@ -31,7 +32,7 @@ export default function Post({ postData }) {
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
